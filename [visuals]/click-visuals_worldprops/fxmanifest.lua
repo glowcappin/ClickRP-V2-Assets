e5b5a1028a9a299f5e3fd42ab0677ed891535d6f
Glow <glowcappin@gmail.com>
2025-08-01 20:34:19 -0400

    upd:(graphics 1)

---
+++ [visuals]/click-visuals_worldprops/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'ANTUNES'
+this_is_a_map "yes"
+
+files {
+	"stream/*.ytyp",
+	"stream/*.ytd",
+    "stream/*.ydr",
+    "stream/*.ydd",
+    "stream/*.ycd",
+    "stream/*.ymap",
+    "stream/*.ymf",
+	
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_bench_02.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_bench_06.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_bench_01a.ytyp'
+
+dependency '/assetpacks'
---
+++ [visuals]/click-visuals_worldprops2/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'ANTUNES'
+description 'Karma Developments - Graphics - World Props'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+lua54 'yes'
+
+dependency '/assetpacks'
