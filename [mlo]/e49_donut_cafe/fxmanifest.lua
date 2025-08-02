9e863e02af39cbefb33cc7a770f74603b6e152ac
Glow <glowcappin@gmail.com>
2025-08-01 20:37:48 -0400

    upd:(last of graphics and props)

---
+++ [mlo]/e49_donut_cafe/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'e_49'
+description 'Rusty Browns donut cafe MLO'
+version '1.0.1'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/customization/*.ytd',
+    'stream/customization/*.ydr'
+}
---
+++ [props]/assets_customprops/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version("cerulean")
+game({ "gta5" })
+author("badcodes")
+description("custom props")
+version("1.0.0")
+
+files({
+	"stream/**/*.ytyp",
+})
+
+data_file("DLC_ITYP_REQUEST")("stream/**/*.ytyp")
