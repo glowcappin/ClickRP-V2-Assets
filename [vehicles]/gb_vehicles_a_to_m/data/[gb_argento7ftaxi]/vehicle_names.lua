6547fc7bf6f69e182f285fbc113d7f2423ed5669
Glow <glowcappin@gmail.com>
2025-08-02 14:05:42 -0400

    upd:(gabz v 5 + np court)

---
+++ [vehicles]/gb_vehicles_a_to_m/data/[gb_argento7ftaxi]/vehicle_names.lua
@@ -0,0 +1,50 @@
+Citizen.CreateThread(function()
+AddTextEntry("TAXIARGENTO", "Taxi Argento 7F")
+
+AddTextEntry("TAXIARGENTO7F_LIV1", "Downtown Cab Co")
+
+AddTextEntry("ARGENTO7F_EXHAUST_1", "Titanium Exhaust")
+AddTextEntry("ARGENTO7F_EXHAUST_2", "Racing Exhaust")
+
+AddTextEntry("ARGENTO7F_SPOILER_1", "Black Lip Spoiler")
+AddTextEntry("ARGENTO7F_SPOILER_2", "Secondary Paint Lip Spoiler")
+AddTextEntry("ARGENTO7F_SPOILER_3", "Carbon Lip Spoiler")
+
+AddTextEntry("ARGENTO7F_SKIRT_1", "Secondary Paint Skirt")
+AddTextEntry("ARGENTO7F_SKIRT_2", "Carbon Skirt")
+
+AddTextEntry("ARGENTO7F_MIRRORS_1", "Secondary Paint Mirrors")
+AddTextEntry("ARGENTO7F_MIRRORS_2", "Carbon Mirrors")
+
+AddTextEntry("ARGENTO7F_HOOD_1", "Carbon Hood")
+
+AddTextEntry("ARGENTO7F_TRIM_1", "Black Trim")
+
+AddTextEntry("ARGENTO7F_ROOF_0", "Delete Flush Rails")
+AddTextEntry("ARGENTO7F_ROOF_1", "Carbon Roof")
+AddTextEntry("ARGENTO7F_ROOF_2", "Carbon Roof without Rails")
+
+AddTextEntry("ARGENTO7F_RBUMPER_1", "Vented Rear Bumper w/ Carbon Fiber Diffuser (Silver)")
+AddTextEntry("ARGENTO7F_RBUMPER_2", "Vented Rear Bumper w/ Carbon Fiber Diffuser (Secondary)")
+AddTextEntry("ARGENTO7F_RBUMPER_3", "Vented Rear Bumper w/ Carbon Fiber Diffuser")
+AddTextEntry("ARGENTO7F_RBUMPER_4", "Rear Bumper w/ Carbon Fiber Diffuser (Secondary)")
+AddTextEntry("ARGENTO7F_RBUMPER_5", "Rear Bumper w/ Carbon Fiber Diffuser")
+
+AddTextEntry("ARGENTO7F_FBUMPER_1", "Blacked Out Front Bumper")
+AddTextEntry("ARGENTO7F_FBUMPER_2", "Carbon Top Front Bumper")
+AddTextEntry("ARGENTO7F_FBUMPER_3", "Black Top Front Bumper")
+AddTextEntry("ARGENTO7F_FBUMPER_4", "Black Bottom Front Bumper")
+
+AddTextEntry("ARGENTO7F_FBUMPERVENTS_1", "Secondary Paint Bumper Vent")
+AddTextEntry("ARGENTO7F_FBUMPERVENTS_2", "Chrome Bumper Vent")
+
+AddTextEntry("ARGENTO7F_FBUMPERTRIM_1", "Secondary Paint Bumper Trim")
+AddTextEntry("ARGENTO7F_FBUMPERTRIM_2", "Carbon Bumper Trim")
+AddTextEntry("ARGENTO7F_FBUMPERTRIM_3", "Chrome Bumper Trim")
+
+AddTextEntry("ARGENTO7F_FBUMPERGRILLES_1", "Secondary Paint Grille")
+AddTextEntry("ARGENTO7F_FBUMPERGRILLES_2", "Dechromed Grille")
+
+AddTextEntry("ARGENTO7F_PLATE_0", "Delete Front Plate")
+AddTextEntry("ARGENTO7F_PLATE_1", "Euro Plates")
+end)
