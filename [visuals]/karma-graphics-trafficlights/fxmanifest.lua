918b2e665d3ea11815a3f7f7a206faef83851981
Glow <glowcappin@gmail.com>
2025-08-02 14:14:59 -0400

    upd:(karma readd)

---
+++ [visuals]/karma-graphics-trafficlights/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'ANTUNES'
+description 'Karma Developments Traffic Lights 4.0'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
+dependency '/assetpacks'
