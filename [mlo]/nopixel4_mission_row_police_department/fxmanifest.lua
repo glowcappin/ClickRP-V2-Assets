d0a946d4edd33877b69082b6b84cb9a637c58346
Glow <glowcappin@gmail.com>
2025-08-02 14:06:48 -0400

    upd:(nopixel mrpd)

---
+++ [mlo]/nopixel4_mission_row_police_department/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'NVE Team'
+description 'Mission Row Police Department'
+version '1.0.0'
+
+-- HTTPS://PRIMELEAKS.CC
+-- HTTPS://PRIMELEAKS.CC
+-- HTTPS://PRIMELEAKS.CC
+-- HTTPS://PRIMELEAKS.CC
+
+this_is_a_map 'yes'
+lua54 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'np4_mrpd_timecycles.xml'
+data_file "GTXD_PARENTING_DATA" "stream/gtxd.meta"
+
+client_scripts {
+  "client.lua"
+  }
+
+files {
+  'np4_mrpd_timecycles.xml',
+  'stream/gtxd.meta'
+}
