5798646904e932a473bd026a711eb4c9431d249a
Glow <glowcappin@gmail.com>
2025-08-01 20:35:40 -0400

    upd:(more graphics)

---
+++ [visuals]/click-visuals_forest/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'ANTUNES'
+description 'Karma Developments - Forest for Graphics overhaul'
+this_is_a_map 'yes'
+
+lua54 'yes'
+
+dependency '/assetpacks'
