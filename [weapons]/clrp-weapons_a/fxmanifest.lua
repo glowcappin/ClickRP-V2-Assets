cc6bf8433664650308382c75f576ff0d2d7769e4
Glow <glowcappin@gmail.com>
2025-08-05 03:52:28 -0400

    upd:(boom)

---
+++ [weapons]/clrp-weapons_a/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script 'main.lua'
+
+files{
+	'**/*.dat',
+	'**/*.meta',
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' '**/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' '**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' '**/pedpersonality.meta'
+--data_file 'WEAPONINFO_FILE_PATCH' '**/weapons.meta'
+--data_file 'WEAPONINFO_FILE_PATCH' 'vanillaguns/*.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons.meta'
+data_file "LOADOUTS_FILE" "**/loadouts.meta"
+data_file "EXPLOSION_INFO_FILE" "**/explosion.meta"
+data_file "EXPLOSIONFX_FILE" "**/explosionfx.dat"
+--data_file 'WEAPONINFO_FILE' '**/vanilla.meta'
+data_file 'WEAPONINFO_FILE' '**/weaponheavypistol.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons_carbinerifle_mk2.meta'
+data_file 'WEAPONINFO_FILE' '**/weapons_doubleaction.meta'
+--data_file 'WEAPONINFO_FILE_PATCH' '**/weaponheavypistol.meta'
+--data_file 'WEAPONINFO_FILE_PATCH' '**/weapons_carbinerifle_mk2.meta'
+--data_file 'WEAPONINFO_FILE_PATCH' '**/weapons_doubleaction.meta'
---
+++ [weapons]/clrp-weapons_b/cl_weaponNames.lua
@@ -0,0 +1,1 @@
+AddTextEntry("WEAPON_P90FM", "P90")
